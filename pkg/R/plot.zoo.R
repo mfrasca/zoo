87afa9e42b68eb25412464e3363dad94ef64570e
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-11 17:31:24 +0000

    plot.zoo now accepts reversed axes for plot.type = "multipe" as well
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo@376 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.zoo.R
+++ pkg/R/plot.zoo.R
@@ -118,12 +118,14 @@ plot.zoo <- function(x, y = NULL, screens = 1,
       panel.number <- j
       range. <- rep(ranges[[j]], length.out = length(time(x)))
       if(j%%nr==0 || j == length(levels(screens))) {
-	args <- list(x.index, range., xlab = "", ylab = ylab[j], xlim = xlim, ...)
+	args <- list(x.index, range., xlab = "", ylab = ylab[j], 
+		xlim = xlim, ylim = ylim[[j]], ...)
 	args$type <- "n"
 	do.call("plot", args)
 	mtext(xlab, side = 1, line = 3)
       } else {      
-        args <- list(x.index, range., axes = FALSE, xlab = "", ylab = ylab[j], xlim = xlim, ...)
+        args <- list(x.index, range., axes = FALSE, xlab = "", ylab = ylab[j], 
+		xlim = xlim, ylim = ylim[[j]], ...)
 	args$type <- "n"
 	do.call("plot", args)
         box()
