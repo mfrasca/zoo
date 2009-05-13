352281270d66db0a29a6c5731a0b68b08171812f
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-13 01:47:54 +0000

    fixed bug in na.trim (Thanks to Spencer Grave for pointing out)
    git-svn-id: svn://r-forge.r-project.org/svnroot/zoo@590 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/na.trim.R
+++ pkg/R/na.trim.R
@@ -3,8 +3,9 @@ na.trim.default <- function (object, sides = c("both", "left", "right"),
 	is.na = c("any", "all"), ...)
 {
    is.na <- match.arg(is.na)
-   nisna <- if (is.na == "any" || length(object) < 2)  complete.cases(object)
-   else rowSums(!is.na(object)) > 0
+   nisna <- if (is.na == "any" || length(dim(object)) < 2)  {
+	complete.cases(object)
+   } else rowSums(!is.na(object)) > 0
    idx <- switch(match.arg(sides), left = cumsum(nisna) > 0,
        right = rev(cumsum(rev(nisna) > 0)>0),
        both = (cumsum(nisna) > 0) & rev(cumsum(rev(nisna)) > 0))
